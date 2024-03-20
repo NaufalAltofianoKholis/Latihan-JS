e49b37c6947c8209f0e5fb6323b1c7ce438ace62
NaufalAltofianoKholis <140329480+NaufalAltofianoKholis@users.noreply.github.com>
2024-03-20 17:16:12 +0700

    new

---
+++ vid7/script.js
@@ -0,0 +1,79 @@
+const hari =1;
+let hasil;
+
+switch (hari) {
+    case 1:
+        hasil="minggu";
+        break;
+    case 2:
+        hasil="senin";
+        break;
+    case 3:
+        hasil="selasa";
+        break;
+    case 4:
+        hasil="rabu";
+        break;
+    case 5:
+        hasil="kamis";
+        break;
+    case 6:
+        hasil="jum'at";
+        break;
+    case 7:
+        hasil="sabtu";
+        break;
+
+    default:
+        hasil ="hari tidak ada";
+        break;
+}
+
+console.log(hasil);
+
+const bulan = "agustus";
+let hasil2;
+
+switch (bulan) {
+    case 'januari':
+        hasil2= "bulan 1";
+        break;
+    case 'februari':
+        hasil2= "bulan 2";
+        break;
+    case 'maret':
+        hasil2= "bulan 3";
+        break;
+    case 'april':
+        hasil2= "bulan 4";
+        break;
+    case 'mei':
+        hasil2= "bulan 5";
+        break;
+    case 'juni':
+        hasil2= "bulan 6";
+        break;
+    case 'juli':
+        hasil2= "bulan 7";
+        break;
+    case 'agustus':
+        hasil2= "bulan 8";
+        break;
+    case 'september':
+        hasi2= "bulan 9";
+        break;
+    case 'oktober':
+        hasil2= "bulan 10";
+        break;
+    case 'november':
+        hasil2= "bulan 11";
+        break;
+    case 'desember':
+        hasil2= "bulan 12";
+        break;
+
+    default:
+        hasil2 ="bulan tidak ada";
+        break;
+}
+console.log(hasil2);
